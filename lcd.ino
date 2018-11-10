e06056cec8225c3fb9f152b910a52720fc8209ec
OmarEl-Ganayni <o.elganayni@gmail.com>
2018-11-10 16:45:21 +0200

    lcd.ino

---
+++ lcd.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h> 
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+void setup() {
+  Serial.begin(9600); // Default connection rate for my BT module`
+
+}
+
+void loop() {
+  
+ if(Serial.available() ){
+lcd.write(Serial.read());
+ }
+}
+
